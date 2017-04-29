90a31ed69e1d014f68765880d92bb19bbf2d0771
Theo Sherman <theo.a.sherman@gmail.com>
2017-04-29 17:22:22 -0500

    added better styling and validation.

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,32 +1,48 @@
 <template lang="pug">
-mixin textbox(binding, label)
+
+mixin input(binding, label)
   .field
     label=label
-    input(type='text', v-model.number=binding, placeholder=label)
+    input(type='number', v-model.number=binding, placeholder=label)
 
-div
-  h1.ui.center.aligned.header Save My Ass Calculator
+.ui.container
+  h1.ui.huge.center.aligned.header Insulin Calculator
   
-  .ui.grid.divided
-    .ten.wide.column
-      h1 inputs
-      .ui.form
-        +textbox('currentBloodSugar', 'Current Blood Sugar')
-        +textbox('targetBloodSugar', 'Target Blood Sugar')
-        +textbox('correction', 'Correction')
-        +textbox('carbIntake', 'Carb Intake')
-        +textbox('carbRatio', 'Carb Ratio')
-        +textbox('insulinUnitSize', 'Insulin Unit Size')
+  .ui.two.column.grid.stackable
+    .column
+      .ui.segment.raised.padded
+        .ui.form
+          +input('currentBloodSugar', 'Current Blood Sugar')
+          +input('carbIntake', 'Carb Intake')
 
-    .six.wide.column
-      h1 result
-      h3 {{ insulinUnits }}
+          .ui.toggle.checkbox
+            input(type='checkbox', v-model='showAdditionalInputs')
+            label Additional Inputs
+          
+          div(v-show='showAdditionalInputs')
+            +input('targetBloodSugar', 'Target Blood Sugar')
+            +input('correction', 'Correction')
+            +input('carbRatio', 'Carb Ratio')
+            +input('insulinUnitSize', 'Insulin Unit Size')
+
+    .column
+      .ui.segment.raised.center.aligned
+        .ui.large.statistic
+          .value {{ insulinUnitsValue }}
+          .label Units of Insulin
 
 </template>
 
 <script>
+import numeral from 'numeral'
+
+function isNumeric(val) {
+  return !Number.isNaN(val) && Number.isFinite(val)
+}
+
 export default {
   data: () => ({
+    showAdditionalInputs: false,
     currentBloodSugar: '',
     targetBloodSugar: '',
     correction: '',
@@ -38,7 +54,40 @@ export default {
   computed: {
     insulinUnits() {
       return ((this.currentBloodSugar - this.targetBloodSugar) / this.correction + (this.carbIntake / this.carbRatio)) / (this.insulinUnitSize / 100)
+    },
+    insulinUnitsValue() {
+      return this.validInsulinUnits ? numeral(this.insulinUnits).format('0.0') : '¯\\_(ツ)_/¯'
+    },
+    validInsulinUnits() {
+      return isNumeric(this.currentBloodSugar) && isNumeric(this.carbIntake) && isNumeric(this.targetBloodSugar) && isNumeric(this.correction) && isNumeric(this.carbRatio) && isNumeric(this.insulinUnitSize) && isNumeric(this.insulinUnits) && this.insulinUnits > 0
     }
-  }
+  },
+
+  watch: {
+    targetBloodSugar: val => localStorage.targetBloodSugar = val,
+    correction: val => localStorage.correction = val,
+    carbRatio: val => localStorage.carbRatio = val,
+    insulinUnitSize: val => localStorage.insulinUnitSize = val,
+  },
+
+  created() {
+    this.targetBloodSugar = Number.parseInt(localStorage.targetBloodSugar)
+    this.correction = Number.parseInt(localStorage.correction)
+    this.carbRatio = Number.parseInt(localStorage.carbRatio)
+    this.insulinUnitSize = Number.parseInt(localStorage.insulinUnitSize)
+
+    this.showAdditionalInputs = !(isNumeric(this.targetBloodSugar) && isNumeric(this.correction) && isNumeric(this.carbRatio) && isNumeric(this.insulinUnitSize))
+    }
 }
 </script>
+
+<style>
+h1.ui.huge.center.aligned.header {
+  margin-top: 0.5em;
+  font-size: 3em;
+}
+
+.ui.toggle.checkbox {
+  margin: 10px 0 20px 0;
+}
+</style>
