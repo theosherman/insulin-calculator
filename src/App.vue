f74b9dbb3d1a49f3518ba626843b44c68b5c0d4e
Theo Sherman <theo.a.sherman@gmail.com>
2017-04-30 17:43:40 -0500

    tweaked to only include necessary css components.

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,13 @@ div
 </template>
 
 <script>
-require("semantic-ui-css/semantic.css")
+require("semantic-ui-css/components/reset.css")
+require("semantic-ui-css/components/container.css")
+require("semantic-ui-css/components/grid.css")
+require("semantic-ui-css/components/site.css")
+require("semantic-ui-css/components/segment.css")
+require("semantic-ui-css/components/header.css")
+require("semantic-ui-css/components/form.css")
+require("semantic-ui-css/components/statistic.css")
+require("semantic-ui-css/components/checkbox.css")
 </script>
